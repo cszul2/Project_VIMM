caaf07aabcd0ef280f3d0b69fe659206655c9114
cszul2 <thisisthechris100@gmail.com>
2020-08-29 00:19:06 -0400

    Added Image python file

---
+++ Image.py
@@ -0,0 +1,27 @@
+import numpy as np
+import scipy.signal
+
+class Image(object):
+    def __init__(self):
+        pass
+
+    def loadImage(self):
+        pass
+
+    def saveImage(self):
+        pass
+
+    def calculateFFT(self):
+        pass
+
+    def applyGaussianWindow(self):
+        pass
+
+    def applyHammingWindow(self):
+        pass
+
+    def covertToGrayscale(self):
+        pass
+
+    def applyCLAHE(self):
+        pass
